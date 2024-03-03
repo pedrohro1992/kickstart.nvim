0dd6f2e7bbe02b6bdcdf1dae16837ad4394d8005
Pedro Oliveira <pedro_hro1992@outlook.com>
2024-03-03 19:07:43 -0300

    Adiciona transparencia no colorscheme

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -8,6 +8,7 @@ return {
 			require('onedark').setup {
 				-- Set a style preset. 'dark' is default.
 				style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+				transparent = true,
 			}
 			require('onedark').load()
 		end,

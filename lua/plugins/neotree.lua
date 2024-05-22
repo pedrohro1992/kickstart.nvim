e8c63e243b1c84154cf93326b45480dba6388495
pedrohro1992 <pedro_hro1992@outlook.com>
2024-05-21 22:38:18 -0300

    Feat - Adiciona configuracao Netrw Hijack no NeoTree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,13 @@ return {
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		opts = {
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true
+			},
 		}
 	}
 }

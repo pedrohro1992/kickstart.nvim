2cba329bcfc25ec1797e2c7aac0ec215e53cb8ee
Pedro Ramos <pedrooramos@protonmail.com>
2026-06-15 23:52:59 -0300

    fix: use real nvim-web-devicons instead of mini.icons mock
    The mini.icons mock for nvim-web-devicons was causing square glyphs in
    barbecue.nvim. Restored the real nvim-tree/nvim-web-devicons as the icon
    provider for all plugins (neo-tree, telescope, lualine-sf) and removed
    the package.preload mock. mini.icons is kept for its custom file icon
    overrides (.keep, devcontainer.json, dotenv) used by mini.nvim consumers.

--- lua/custom/plugins/mini-icons.lua
+++ lua/custom/plugins/mini-icons.lua
@@ -1,7 +1,6 @@
 return {
   {
     'echasnovski/mini.icons',
-    lazy = true,
     opts = {
       file = {
         ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
@@ -11,11 +10,5 @@ return {
         dotenv = { glyph = '', hl = 'MiniIconsYellow' },
       },
     },
-    init = function()
-      package.preload['nvim-web-devicons'] = function()
-        require('mini.icons').mock_nvim_web_devicons()
-        return package.loaded['nvim-web-devicons']
-      end
-    end,
   },
 }
